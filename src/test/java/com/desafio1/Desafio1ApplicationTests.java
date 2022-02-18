fdc53f85025b3f552830dd41460818848663f18a
samuel <samueln.garciar@gmail.com>
2022-02-18 17:19:04 +0000

    junit test fix

--- src/test/java/com/desafio1/Desafio1ApplicationTests.java
+++ src/test/java/com/desafio1/Desafio1ApplicationTests.java
@@ -2,7 +2,7 @@ package com.desafio1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.hamcrest.MatcherAssert.*;
+ import static org.junit.Assert.*;
 
 @SpringBootTest
 class Desafio1ApplicationTests {
