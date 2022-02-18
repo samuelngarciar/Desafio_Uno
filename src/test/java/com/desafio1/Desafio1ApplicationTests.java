0321197ee3d4cbbf814f419029e9b0046f7e1097
samuel <samueln.garciar@gmail.com>
2022-02-18 17:16:29 +0000

    junit test

--- src/test/java/com/desafio1/Desafio1ApplicationTests.java
+++ src/test/java/com/desafio1/Desafio1ApplicationTests.java
@@ -2,6 +2,7 @@ package com.desafio1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.MatcherAssert.*;
 
 @SpringBootTest
 class Desafio1ApplicationTests {
@@ -9,5 +10,12 @@ class Desafio1ApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+
+
+   @Test
+    public void testCustom() {
+      assertTrue("Is equal compare", 1==2);
+    }
 
 }
